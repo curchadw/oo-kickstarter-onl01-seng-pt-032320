4c798e1a8ea2747006cb2d19e2aa7ed446fe9b3f
Curtis Chadwell <laurencechadwell@gmail.com>
2020-04-09 23:25:01 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,7 +6,7 @@ class Backer
     @backed_projects = []
   end
 
-  def back_project(Project)
-    @backed_projects.push(Project)
+  def back_project(project)
+    @backed_projects.push(project)
   end
 end
